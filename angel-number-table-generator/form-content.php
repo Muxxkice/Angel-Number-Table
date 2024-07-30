40413211f94f8be0624fcf0227a0b9a23b9c3f25
mao.kurihara <mao.kurihara@rensa.co.jp>
2024-07-30 14:23:05 +0900

    文言の追加等

--- angel-number-table-generator/form-content.php
+++ angel-number-table-generator/form-content.php
@@ -1,4 +1,10 @@
-<div class="wrap">
+<?php
+  if (!current_user_can('manage_options')) {
+      wp_die(__('You do not have sufficient permissions to access this page.'));
+  }
+?>
+
+<div class="antg-wrap">
   <h1>エンジェルナンバーの管理</h1>
   <?php
     settings_fields('angel_number_settings');
@@ -8,24 +14,27 @@
 
   <?php if (isset($_GET['message'])) : ?>
     <?php if ($_GET['message'] == MESSAGE_SUCCESS) : ?>
-        <div class="updated"><p><strong>設定を保存しました</strong></p></div>
+        <div class="updated"><p><strong><?php echo esc_html__('設定を保存しました', 'text-domain'); ?></strong></p></div>
     <?php elseif ($_GET['message'] == MESSAGE_INVALID_INPUT) : ?>
-        <div class="error"><p><strong>無効な文字が含まれています。半角数字のみを入力してください。</strong></p></div>
+        <div class="error"><p><strong><?php echo esc_html__('無効な文字が含まれています。半角数字のみを入力してください。', 'text-domain'); ?></strong></p></div>
     <?php elseif ($_GET['message'] == MESSAGE_DUPLICATE_ENTRY) : ?>
-        <div class="error"><p><strong>このエンジェルナンバーは既に存在します。</strong></p></div>
+        <div class="error"><p><strong><?php echo esc_html__('このエンジェルナンバーは既に存在します。', 'text-domain'); ?></strong></p></div>
     <?php endif; ?>
   <?php endif; ?>
 
   <form method="post" action="admin-post.php?action=save_angel_numbers">
     <?php wp_nonce_field('save_angel_number_action', '_wpnonce_save_angel_number'); ?>
+    <h3>■エンジェナンバーの追加</h3>
     <table>
-      <tr><label>エンジェルナンバーの追加</label></tr>
+      <tr><label>追加したい数字(半角英数字)</label></tr>
       <td><input type="text" name="new_angel_number" id="new_angel_number" value="" /></td>
     </table>
     <input type="submit" name="add_angel_number" id="add_angel_number" class="button button-primary" value="追加" disabled/>
   </form>
 
   <form method="post" action="admin-post.php?action=delete_angel_numbers">
+    <h3>■エンジェナンバーの削除</h3>
+    <p>削除したい数字にチェックを入れてください</p>
     <?php wp_nonce_field('delete_angel_numbers_action', '_wpnonce_delete_angel_numbers'); ?>
     <ul>
     <?php sort($numbers); ?>
